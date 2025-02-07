2e07a9e62a65181bb7eb045f6e6222a1d44a0703
hfolcot <hfolcot@gmail.com>
2025-02-07 17:36:22 +0000

    setup jest testing

--- projects/ngx-comma-ui/src/lib/button/button.component.spec.ts
+++ projects/ngx-comma-ui/src/lib/button/button.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ButtonComponent } from './button.component';
+import { By } from '@angular/platform-browser';
 
 describe('ButtonComponent', () => {
   let component: ButtonComponent;
@@ -10,7 +11,7 @@ describe('ButtonComponent', () => {
     await TestBed.configureTestingModule({
       imports: [ButtonComponent]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(ButtonComponent);
     component = fixture.componentInstance;
@@ -20,4 +21,36 @@ describe('ButtonComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it("should emit click event when clicked", () => {
+    const spy = jest.spyOn(component.click, "emit");
+
+    const button = fixture.debugElement.query(By.css("[data-testid='button']"));
+    button.nativeElement.click();
+
+    fixture.detectChanges();
+
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
+
+  it("should not be clickable when disabled", () => {
+    const spy = jest.spyOn(component.click, "emit");
+
+    fixture.componentRef.setInput("disabled", true);
+    const button = fixture.debugElement.query(By.css("[data-testid='button']"));
+    button.nativeElement.click();
+
+    fixture.detectChanges();
+
+    expect(spy).not.toHaveBeenCalled();
+  });
+
+  it("should apply the disabled class when disabled", () => {
+    fixture.componentRef.setInput("disabled", true);
+    fixture.detectChanges();
+    const button = fixture.debugElement.query(By.css("[data-testid='button']"));
+
+    expect(button.nativeElement.classList.contains("disabled")).toBe(true);
+
+  })
 });
